6ff2fca0e569799a37b95c1c552db60444052e2f
vly241 <valerkimle241@gmail.com>
2018-08-21 21:06:40 -0500

    valeria
    modificacion punto 3

--- 3_d.php
+++ 3_d.php
@@ -1,19 +1,19 @@
 <?php
 
 $n1=1;
- $n2=1;
+ 
  $n3=23;
 
  for($n2=1;$n2 <= $n3;$n2++){
      if($n2<=$n3){
-         echo $n2.'+';
+         echo pow(2,$n2). ", ";
      }else{
          echo $n3;
      }
-     $n1=$n2+$n1;
+     $n1=pow(2,$n2);
      
      if($n2==$n3){
-         echo '='.$n1;
+         echo '= '.$n1;
      }
      
  }
